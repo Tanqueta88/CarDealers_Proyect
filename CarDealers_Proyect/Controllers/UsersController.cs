f35d5cb8afe251dd95e315ecc1f78cf0afcd8e80
tanqueta88 <oliverprim783@gmail.com>
2024-04-25 00:34:52 -0300

    UsersController is added

---
+++ CarDealers_Proyect/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+
+
+using guido_sanz_parcial1.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using guido_sanz_parcial1.Services;
+
+namespace guido_sanz_parcial1.Controllers;
+
+[Authorize]
+public class UsersController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IUsersService _usersService;
+
+    public UsersController(
+        ILogger<HomeController> logger,
+        IUsersService usersService)
+    {
+        _logger = logger;
+        _usersService = usersService;
+    }
+
+    [Authorize(Roles = "admin")]
+    public IActionResult Index(string? usernameFilter)
+    {
+        UserSearchViewModel userSearchViewModel;
+        List<IdentityUser> usersList;
+
+        if(!string.IsNullOrEmpty(usernameFilter)){
+                usersList = _usersService.GetAll(usernameFilter);
+            }else{
+                usersList = _usersService.GetAll();
+        }
+        userSearchViewModel = new UserSearchViewModel();
+        userSearchViewModel.Users = usersList;       
+        return View(userSearchViewModel);
+    }
+
+    [Authorize]
+    public async Task<IActionResult> Edit(string id)
+    {
+        var userViewModel = await _usersService.GetById(id);
+        return View(userViewModel);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(UserEditViewModel model)
+    {
+        _usersService.Update(model);
+        return RedirectToAction("Index");
+    }
+}
