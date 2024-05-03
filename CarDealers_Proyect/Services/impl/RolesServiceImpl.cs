c68d884106ef377885afb2b4d710eb61c9fca8b1
tanqueta88 <oliverprim783@gmail.com>
2024-05-02 23:27:07 -0300

    RolesServiceImpls file is added

---
+++ CarDealers_Proyect/Services/impl/RolesServiceImpl.cs
@@ -0,0 +1,30 @@
+using guido_sanz_parcial1.ViewModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace guido_sanz_parcial1.Services;
+
+public class RolesServiceImpl : IRolesService
+{
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public RolesServiceImpl(RoleManager<IdentityRole> roleManager){
+        _roleManager = roleManager;
+    }
+
+    public void create(RoleCreateViewModel obj)
+    {
+        var role = new IdentityRole(obj.RoleName);
+        _roleManager.CreateAsync(role);
+    }
+
+    public List<IdentityRole> GetAll()
+    {
+        var roles = _roleManager.Roles.ToList();
+        return roles;
+    }
+    public List<IdentityRole> GetAll(string namefilter)
+    {
+        var roles = _roleManager.Roles.Where(x => x.Name.ToLower().Contains(namefilter.ToLower())).ToList();
+        return roles;
+    }
+}
